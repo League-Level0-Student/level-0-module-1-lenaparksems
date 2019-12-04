335c03ec3bc21a168d1f221752b85fc10970610d
Lena Park <Park.Lena@SEMS259.LAB>
2019-12-04 14:34:07 -0800

    SAVE

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -6,11 +6,11 @@ package _04_secret_message_box;
 import javax.swing.JOptionPane;
 
 /**
- * Secret Message Box / Secure Messaging System
- * 
+ * Secret Message Box / Secure Messaging
  * You want to leave a message on one of the Windows computers so that only your friend can read it.
  * You set up the passcode and the secret message. 
  * Your friend types in the passcode to retrieve the message.
+ *  System
  * 
  */
 
@@ -18,18 +18,38 @@ public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
 	public static void main(String[] args) {
+	//to tell the NEXT user that they can only see the secret message 
+	//    if they can guess(not match, pop-up "INTRUDER!!"
+
 		
 	
 	// 1. Set a password in a String variable
 	String password = "I am better then you ahahahah you cant guss this password because its too long and complex and i also have to do homework which i hate.....";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-	JOptionPane.showMessageDialog(null, password);
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
- 
+	  String message = JOptionPane.showInputDialog("Type a secret message or alse you die");
+	JOptionPane.showMessageDialog(null, "what is the password?");
+	// 3. Now use a pop-up s the password
+    String guss = JOptionPane.showInputDialog("Type password (HERE)");
 	// 4. If their guess matches the password, show them the secret message
 
-	// 5. If the password does not match, pop-up "INTRUDER!!"
+    
+    if (guss.equals( password )){ 
+		
+		
+		
+		
+	
+JOptionPane.showMessageDialog(null, message);
+}
+    else {
+    	
+    	JOptionPane.showMessageDialog(null,"If password is wrong, slap them in the face");
+    	JOptionPane.showMessageDialog(null, "*slaps*");
+    	JOptionPane.showMessageDialog(null, "try again and get slapped again please");
+    	
+    }
 
-}}
+   	}}
+		
+
 
